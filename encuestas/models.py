79af06e998fe2fbbfd411c14d81299c6e3de83ae
Heiker <heikercuriel@gmail.com>
2016-09-04 18:36:16 -0400

    Parte 2 Tutorial Django

--- encuestas/models.py
+++ encuestas/models.py
@@ -1,10 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 class Pregunta(models.Model):
 	pregunta_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('fecha de publicacion')
 
+	def __str__(self):
+		return self.pregunta_text
+
+	def es_reciente(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Opcion(models.Model):
 	pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
 	opcion_text = models.CharField(max_length=200)
 	votos = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.opcion_text
