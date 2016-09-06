2be865c8ae0b0fb9707059c6a5b85162ab61787f
Heiker <heikercuriel@gmail.com>
2016-09-06 00:13:31 -0400

    Parte 3 tutorial

--- encuestas/urls.py
+++ encuestas/urls.py
@@ -1,6 +1,18 @@
 from django.conf.urls import url
 from . import views
 
+#Namespace para los nombres de las rutas
+app_name = 'encuestas'
 urlpatterns = [
+	# ej: /encuestas/
 	url(r'^$', views.index, name='index'),
+
+	# ej: /encuestas/5/
+	url(r'^(?P<pregunta_id>[0-9]+)/$', views.detalles, name='detalles'),
+	
+	# ej: /encuestas/5/resultados/
+	url(r'^(?P<pregunta_id>[0-9]+)/resultados/$', views.resultados, name='resultados'),
+	
+	# ej: /encuestas/5/votar/
+	url(r'^(?P<pregunta_id>[0-9]+)/votar/$', views.votar, name='votar'),
 ]
--- encuestas/views.py
+++ encuestas/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse as respuesta
+from django.shortcuts import get_object_or_404 as findOrFail, render
+from django.http import HttpResponse as Respuesta, Http404
+from .models import Pregunta
 
 def index(peticion):
-	return respuesta("Hola, mundo. Estas en el index de las Encuestas.")
+	lista = Pregunta.objects.order_by('-pub_date')[:5]
+	context = {
+		'lista': lista
+	}
+	
+	return render(peticion, 'encuestas/index.html', context)
+
+def detalles(peticion, pregunta_id):
+	pregunta = findOrFail(Pregunta, pk=pregunta_id)
+	context = {
+		'pregunta': pregunta
+	}
+
+	return render(peticion, 'encuestas/detalles.html', context)
+
+def resultados(peticion, pregunta_id):
+	mensaje = "Resultado de la pregunta %s."
+	
+	return Respuesta(mensaje % pregunta_id)
+
+def votar(peticion, pregunta_id):
+	return Respuesta("Estas votando en la pregunta %s." % pregunta_id)
